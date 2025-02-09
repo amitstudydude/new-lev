c8bf5248b3db231f79494c17c6c87754345c5389
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2025-02-09 21:58:07 +0530

    Update and rename Kasmweb to Kasmweb.sh

--- Kasmweb.sh
+++ Kasmweb.sh
@@ -3,6 +3,8 @@ cd /tmp
 curl -O https://kasm-static-content.s3.amazonaws.com/kasm_release_1.16.1.98d6fa.tar.gz
 tar -xf kasm_release_1.16.1.98d6fa.tar.gz
 lsof -i :3389
+pid=$(lsof -t -i:3389)
+sudo kill -9 $pid
 sudo bash kasm_release/install.sh --accept-eula --swap-size 4096 --admin-password root -L 7887
 cd /opt/kasm/current/certs
 rm kasm_nginx.crt kasm_nginx.key
