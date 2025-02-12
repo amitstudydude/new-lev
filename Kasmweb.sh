8250a1b79d5eae143431cb5ccc5f229e82c8ea38
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2025-02-12 17:48:22 +0530

    Update Kasmweb.sh

--- Kasmweb.sh
+++ Kasmweb.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-wget https://download.nomachine.com/download/8.16/Linux/nomachine_8.16.1_1_amd64.deb && apt install ./nomachine_8.16.1_1_amd64.deb 
-wget https://openport.io/download/debian64/latest.deb && sudo dpkg -i latest.deb && rm *.deb
-openport 4000
 cd /tmp
 curl -O https://kasm-static-content.s3.amazonaws.com/kasm_release_1.16.1.98d6fa.tar.gz
 tar -xf kasm_release_1.16.1.98d6fa.tar.gz
@@ -11,4 +8,7 @@ sudo kill -9 $pid
 pid=$(lsof -t -i:7887)
 sudo kill -9 $pid
 sudo bash kasm_release/install.sh --accept-eula --swap-size 4096 --admin-password root -L 7887
+wget https://download.nomachine.com/download/8.16/Linux/nomachine_8.16.1_1_amd64.deb && apt install ./nomachine_8.16.1_1_amd64.deb 
+wget https://openport.io/download/debian64/latest.deb && sudo dpkg -i latest.deb && rm *.deb
+openport 4000
 
